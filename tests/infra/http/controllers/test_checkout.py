5c8268b8e384104cb7ef0a3774a3d934cc7f9ed0
Thiago Pereria <tisp.thiago@gmail.com>
2021-06-13 17:32:37 -0300

    fix: fix checkout controller

--- tests/infra/http/controllers/test_checkout.py
+++ tests/infra/http/controllers/test_checkout.py
@@ -2,6 +2,7 @@ import pytest
 from unittest import mock
 import json
 
+from src.domain import Checkout
 from src.infra.http.app import create_app
 
 
@@ -28,14 +29,15 @@ checkout = {
 }
 
 
-@mock.patch("src.infra.http.controllers.checkout.checkout")
+@mock.patch("src.infra.http.controllers.checkout.CalculateCheckout.calculate")
 def test_get(mock_use_case, client):
-    mock_use_case.return_value = checkout
-
+    mock_use_case.return_value = Checkout(**checkout)
+    print(mock_use_case)
     http_response = client.post(
         "/checkout", json={"products": [{"id": 1, "quantity": 3}]}
     )
-
+    print(http_response)
     assert json.loads(http_response.data.decode("UTF-8")) == checkout
+    mock_use_case.assert_called()
     assert http_response.status_code == 201
     assert http_response.mimetype == "application/json"
